d297d16ce7c8e614f1a2ddd0323ecf9c406a8640
EdManDev <ed.manigat@gmail.com>
2020-12-05 15:28:48 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import { Canvas } from "react-three-fiber";
+import {
+	EditableManager,
+	editable as e,
+	configure,
+} from "react-three-editable";
 
-function App() {
+// Import our previously exported state
+import editableState from "./editableState.json";
+
+configure({
+	localStorageNamespace: "minimal",
+});
+
+export default function App() {
 	return (
-		<div>
-			<h1>Hello there</h1>
-		</div>
+		<Canvas>
+			{/* EditableManager connnects this canvas to React Three Editable. Here we can also pass our state. */}
+			<EditableManager state={editableState} />
+			<ambientLight intensity={0.5} />
+			{/* Mark objects as editable. */}
+			{/* Transforms applied in the editor are added on top of  transforms applied in code. */}
+			<e.spotLight
+				position={[10, 10, 10]}
+				angle={0.15}
+				penumbra={1}
+				uniqueName="Spotlight"
+			/>
+			<e.pointLight uniqueName="PointLight" />
+			<e.mesh uniqueName="Box">
+				<boxBufferGeometry />
+				<meshStandardMaterial color="orange" />
+			</e.mesh>
+		</Canvas>
 	);
 }
-
-export default App;
